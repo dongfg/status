d99735345a261446124b01e92511bd2896fa9c53
dongfg <mail@dongfg.com>
2025-05-14 14:54:30 +0800

    feat: commit script

---
+++ checkshell/cnb-commit.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+git checkout page
+git config --local user.name "$CNB_COMMITTER"
+git config --local user.email "$CNB_COMMITTER_EMAIL"
+git add -A --force ./logs/
+git commit -m '🆙 [Automated] Update service status logs'
+git remote set-url origin "https://x-access-token:$GITHUB_REPO_TOKEN@github.com/dongfg/status.git"
+git push
