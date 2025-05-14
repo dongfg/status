16f7b1c689d56355794c6a98f42024c22585aba9
dongfg <mail@dongfg.com>
2025-05-14 15:08:58 +0800

    feat: do not commit log to master

--- checkshell/cnb-commit.sh
+++ checkshell/cnb-commit.sh
@@ -5,5 +5,6 @@ git config --local user.name "$CNB_COMMITTER"
 git config --local user.email "$CNB_COMMITTER_EMAIL"
 git add -A --force ./logs/
 git commit -m '🆙 [Automated] Update service status logs'
+git push -f
 git remote set-url origin "https://x-access-token:$GITHUB_REPO_TOKEN@github.com/dongfg/status.git"
-git push
+git push -f
