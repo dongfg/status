54f4d2ac66591aa0a3e6eb53c682d7c90dd40e82
dongfg <mail@dongfg.com>
2025-05-14 15:25:05 +0800

    fix: fix push

--- checkshell/cnb-commit.sh
+++ checkshell/cnb-commit.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 git fetch
-git checkout origin/page
+git switch page
 git config --local user.name "$CNB_COMMITTER"
 git config --local user.email "$CNB_COMMITTER_EMAIL"
 git add -A --force ./logs/
 git commit -m '🆙 [Automated] Update service status logs'
-git push -f git push origin HEAD:page
+git push -f origin HEAD:page
 git remote set-url origin "https://x-access-token:$GITHUB_REPO_TOKEN@github.com/dongfg/status.git"
-git push -f git push origin HEAD:page
+git push -f origin HEAD:page
